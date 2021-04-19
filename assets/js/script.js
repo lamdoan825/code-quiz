e465da0f4fa9842a0cffa0d742405335db9c0291
lamdoan825@gmail.com <lam@Lams-Air.attlocal.net>
2021-04-18 20:37:56 -0500

    aligned elements in html, css, and started creating the dom for js

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var question1 = ["How do you write 'Hello World' in an alert box?",
+"How do you create a function in JavaScript?",
+"Where is the correct place to insert a JavaScript?",
+"Inside which HTML element do we put the JavaScript?",
+"The external JavaScript file must contain the 'script' tag.",
+"How to write an IF statement in JavaScript?",
+"How does a WHILE loop start?",
+"How can you add a comment in a JavaScript?",
+"What is the correct way to write a JavaScript array?",
+"Which event occurs when the user clicks on an HTML element?"
+];
+
+var options1 = ['<button class=buttons1 onclick=q1i()>alertBox("Hello World");</button><br /><br /><button class=buttons1 onclick=q1c()>alert("Hello World"); </button>'];
+var options2 = ['<button class=buttons1 onclick=q2c()>function myFunction()  </button><br /><br /><button class=buttons1 onclick=q2i()>function = myFunction()</button>'];
+var options3 = ['<button class=buttons1 onclick=q3c()>Both the "head" section and the "body" section are correct  </button><br /><br /><button class=buttons1 onclick=q3i()>The "footer" section</button>'];
+var options4 = ['<button class=buttons1 onclick=q4i()>"js" element</button><br /><br /><button class=buttons1 onclick=q4c()>"script" element</button>'];
+var options5 = ['<button class=buttons1 onclick=q5c()>False</button><br /><br /><button class=buttons1 onclick=q5i()>True</button>'];
+var options6 = ['<button class=buttons1 onclick=q6i()>if i = 5  </button><br /><br /><button class=buttons1 onclick=q6c()>if (i == 5)  </button>'];
+var options7 = ['<button class=buttons1 onclick=q7c()>for (i = 0; i <= 5; i++)  </button><br /><br /><button class=buttons1 onclick=q7i()>for (i <= 5; i++)</button>'];
+var options8 = ['<button class=buttons1 onclick=q8c()>//This is a comment  </button><br /><br /><button class=buttons1 onclick=q8i()>!--This is a comment--</button>'];
+var options9 = ['<button class=buttons1 onclick=q9c()>var colors = ["red", "green", "blue"]  </button><br /><br /><button class=buttons1 onclick=q9i()>var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")</button>'];
+var options10 = ['<button class=buttons1 onclick=q10i()>onchange</button><br /><br /><button class=buttons1 onclick=q10c()>onclick  </button>'];
+
